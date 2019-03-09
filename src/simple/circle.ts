7bb953de46f03ca366ba2926a83bba265e85cd09
blenderskool <akash.hamirwasia@gmail.com>
2019-03-09 13:23:12 +0530

    Added circle element

---
+++ src/simple/circle.ts
@@ -0,0 +1,30 @@
+import Ellipse from './ellipse';
+
+export default class Circle extends Ellipse {
+
+  /**
+   * @param {Object} options options to create a circle
+   * @param {number} options.x x coordinate of the circle
+   * @param {number} options.y y coordinate of the circle
+   * @param {number} options.r radius of the circle
+   * @param {string} options.background fill color
+   * @param {string} options.borderColor border color
+   * @param {number} options.borderWidth border width
+   * @param {CanvasRenderingContext2D} options.ctx canvas context where circle would be drawn
+   */
+  constructor(options :
+    { x: number, y: number, r: number, background?: string, borderColor?: string, borderWidth?: number, ctx: CanvasRenderingContext2D }
+  ) {
+
+    const { x, y, r, background, borderColor, borderWidth, ctx} = options;
+
+    super({
+      x, y,
+      radiusX: r,
+      radiusY: r,
+      rotation: 0,
+      background, borderColor, borderWidth, ctx
+    });
+  }
+
+};
